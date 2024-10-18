030b60a9d521cb4668be25d9a584bf8ee9cb3e78
Minh Chen <longming_chen@cas-pe.com>
2024-10-18 09:27:03 +0800

    提交

--- assembly/bin/supersonic-build.sh
+++ assembly/bin/supersonic-build.sh
@@ -15,8 +15,7 @@ fi
 function buildJavaService {
   model_name=$1
   echo "starting building supersonic-${model_name} service"
-#  mvn -f $projectDir clean package -DskipTests -Dspotless.skip=true
-  mvn -f $projectDir clean package -DskipTests -Dspotless.skip=true -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true --settings settings.xml
+  mvn -f $projectDir clean package -DskipTests -Dspotless.skip=true
   if [ $? -ne 0 ]; then
       echo "Failed to build backend Java modules."
       exit 1
